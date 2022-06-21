d288c685f67b3a2ab92347db7cfb7d11e4fbe930
João victor <jolita.0312@gmail.com>
2022-06-21 16:46:17 -0300

    alterado mensagens de erro e melhorado com cores aplicadas

--- assets/js/script.js
+++ assets/js/script.js
@@ -43,16 +43,26 @@ function CriaCadastro(){
     let inpContato = document.getElementsByName("contato")[0].value
     let inpTelefone = document.getElementsByName("telefone")[0].value
     let inpCargo = document.getElementsByName("cargo")[0].value
-    let testando = validaIdadeEmail(inpDataNascimento,inpEmail)
+    let testandoIdade = validaIdade(inpDataNascimento)
+    let testandoEmail = validaEmail(inpEmail)
     let Usuario = new Pessoa(inpNome,inpSobrenome,inpDataNascimento,inpEmail,inpContato,inpTelefone,inpCargo)
-    if(testando === true){
+    
+    if(testandoIdade === true && inpDataNascimento != '' && testandoEmail === true){
         banco.push(Usuario)
         Pessoa.addStatico(inpNome,inpSobrenome,inpEmail,inpCargo)
     }
-    else{
-        aparecerPopUp()
-        fecharAuto() 
+    else if( testandoEmail === false){
+        aparecerPopUp2()
+        fecharAuto2()
     }
+    else{
+        aparecerPopUp1()
+        fecharAuto1() 
+    }
+
+console.log(testandoIdade, testandoEmail, inpDataNascimento);
+
+
 }
 
 function stopDefAction(evt) {
@@ -66,7 +76,7 @@ function filtro(){
     let lateral = document.querySelector("#lista-de-alunos")
     const tipoFiltro = document.querySelector("#cargoOption").value
     lateral.innerHTML = ""
-    if(tipoFiltro == "Todos"){
+    if(tipoFiltro === "Todos"){
         banco.forEach(function(usuario){
             Pessoa.addStatico(usuario.nome,usuario.sobrenome,usuario.email,usuario.cargo)
         })
@@ -82,42 +92,65 @@ function filtro(){
 }
       
 }
-function validaIdadeEmail(idade,email){
+function validaIdade(idade){
     let data = new Date()
     let anos = data.getFullYear()
 
     let idadeEmAnos = idade.split("-")[0]
-    let testeEmail = banco.find(function(elemento){return elemento.email === email})
 
-    if(anos - idadeEmAnos >= 18 && testeEmail === undefined && email !== ""){
+    if((anos - idadeEmAnos) >= 18){
+        return true
+    }
+    else{
+        return false
+    }
+}
+
+function validaEmail(email){
+    let testeEmail = banco.find(function(elemento){return elemento.email === email})
+    if(testeEmail === undefined && email !== ""){
         return true
     }
     else{return false}
 }
 
-let PopUp = document.querySelector(".modal-conteiner")
+let container = document.querySelector(".modal-conteiner")
+let popUp1 = document.querySelector(".popUp1")
+let butPop1 = document.querySelector(".buttonPop")
+butPop1.addEventListener("click",fechar1)
 
-let butPop = document.querySelector(".buttonPop")
-butPop.addEventListener("click",fechar)
+let popUp2 = document.querySelector(".popUp2")
+let butPop2 = document.querySelector(".buttonPop")
+butPop2.addEventListener("click",fechar2)
 
-function aparecerPopUp(){
-    PopUp.style.display = "flex"
+function aparecerPopUp1(){
+    container.style.display = "flex"
+    popUp1.style.display = "flex"
 }
-function fechar(){
-    PopUp.style.display = "none"
+function fechar1(){
+    container.style.display = "none"
+    popUp1.style.display = "none"
 }
-function fecharAuto(){
+function fecharAuto1(){
     setTimeout(()=>{
-        PopUp.style.display = "none"
+        container.style.display = "none"
+        popUp1.style.display = "none"
     },10000)
 }
-
-//UsersPara Teste de Filtro
-// let user1 = new Pessoa("João Victor","Lemos","04-08-1999","joao_victorlemos@hotmail.com","31912129231","1158965412","Aluno")
-// let user2 = new Pessoa("Zequinha","Francisco","06-09-1998","zequinha@kenzie.com.br","19183211621","19183211621","Facilitador")
-// let user3 = new Pessoa("João","Goulart","30-05-2000","joaogl@kenzie.com.br","59874211621","17985181621","Instrutor")
-//  banco = [user1,user2,user3]
-// filtro()
+function aparecerPopUp2(){
+    container.style.display = "flex"
+    popUp2.style.display = "flex"
+}
+function fechar2(){
+    container.style.display = "none"
+    popUp2.style.display = "none"
+}
+function fecharAuto2(){
+    setTimeout(()=>{
+        container.style.display = "none"
+        popUp2.style.display = "none"
+    },10000)
+}
 
     let totalAlunos = document.getElementById("total-alunos")
 
@@ -125,5 +158,4 @@ function fecharAuto(){
         let teste = document.querySelectorAll("li").length
         totalAlunos.innerText = `${banco.length}`
     }
-
     atualizaTotal()
